1f70aca076503c0b3162be00a076d9475db0cd82
Peter <hewmorist@gmail.com>
2024-12-28 08:04:34 +0000

    moved port setting out of Dockerfile

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -3,6 +3,10 @@
 # restart udev so for new rules
 # NOTE: Need to issue usbipd attach --wsl --<busid> from Windows powershell 
 
+sudo rm -f /dev/ttyUSB0
+sudo mknod /dev/ttyUSB0 c 188 0
+sudo chmod 666 /dev/ttyUSB0
+sudo usermod -a -G dialout vscode
 sudo service udev restart
 
 
